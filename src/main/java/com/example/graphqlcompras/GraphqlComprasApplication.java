05f325d1b0b6a4eaed3c6c2d94e95514f6ec0a0e
Aramiz Moura <aramiz.moura.gyn@gmail.com>
2022-09-28 21:50:22 -0300

    first commit

---
+++ src/main/java/com/example/graphqlcompras/GraphqlComprasApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqlcompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlComprasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlComprasApplication.class, args);
+    }
+
+}
