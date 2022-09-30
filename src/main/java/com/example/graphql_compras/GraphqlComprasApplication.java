9cb3e86d93568da6baba0bbb25cd8164395f46a5
Aramiz Moura <aramiz.moura.gyn@gmail.com>
2022-09-29 23:27:52 -0300

    initial features for graphql

---
+++ src/main/java/com/example/graphql_compras/GraphqlComprasApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphql_compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlComprasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlComprasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/graphql_compras/GraphqlComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphql_compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlComprasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
